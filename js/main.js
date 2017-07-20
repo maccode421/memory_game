79fe0c3103308688ee5b65a78546d948248f7318
maccode421 <maccavalli@yahoo.com>
2017-07-19 20:54:55 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+var cardOne = [0];
+cardsInPlay.push('cardOne');
+console.log('User flipped queen')
+
+var cardTwo = [2];
+cardsInPlay.push('cardTwo');
+console.log('User flipped king')
+// Code to be executed if condition1 is true
+// come back and fix these
+if (cardsInPlay.length === 2) {
+  alert('You found a match!');
+}  else if (cardsInPlay[0] === cardsInPlay[2]) {
+    alert('You found a match!');
+}  else (cardsInPlay[0] !== cardsInPlay[2]) {
+    alert('Sorry, try again.');
+}
