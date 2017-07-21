a2f26eddd72f6128ee9bd021285198ded2d8984b
maccode421 <maccavalli@yahoo.com>
2017-07-20 23:49:23 -0400

    index.html

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-
-var cardOne = [0];
-cardsInPlay.push('cardOne');
-console.log('User flipped queen')
-
-var cardTwo = [2];
-cardsInPlay.push('cardTwo');
-console.log('User flipped king')
-// Code to be executed if condition1 is true
-// come back and fix these
-if (cardsInPlay.length === 2) {
-  alert('You found a match!');
-}  else if (cardsInPlay[0] === cardsInPlay[2]) {
-    alert('You found a match!');
-}  else (cardsInPlay[0] !== cardsInPlay[2]) {
-    alert('Sorry, try again.');
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
 }
+}
+var flipCard = function(cards[0], cards[2]) {
+console.log('User flipped' + cards[0]);
+cardsInPlay.push(cards[0]);
+flipCard();
+flipCard(2);
+checkForMatch();
+  /*var cardOne = cards[0];
+  cardsInPlay.push(cardOne);
+  console.log('User flipped' + cardOne);
+
+  var cardTwo = cards[2];
+  cardsInPlay.push(cardTwo);
+  console.log('User flipped' + cardTwo);*/
+
+  // come back and fix these codes
+  if (cardsInPlay.length === 2) {
+    (cardOne[0] === cardOne[1])
+    alert('You found a match!');
+  }  else (cardOne[0] !== cardTwo[2]) {
+      alert('Sorry, try again.');
+  }
+
+};
